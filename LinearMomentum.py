da2155731551569e75bfed3c5e98bb30a8434151
Hjy1234r <binhluong1968@gmail.com>
2026-09-01 19:03:34 +0700

    Update LinearMomentum.py

--- LinearMomentum.py
+++ LinearMomentum.py
@@ -1,16 +1,46 @@
 import matplotlib.pyplot as plt
 import streamlit as st
-m1 = st.number_input("m1 (kg)", 0.1, 5.0, 0.1)
-m2 = st.number_input("m2 (kg)", 0.1, 5.0, 0.1)
-m1A = st.number_input("v1 (m/s)", -10.0, 10.0, 0.0)
-m2A = st.number_input("v2 (m/s)", -10.0, 10.0, 0.0)
-m1 = st.slider("m1 (kg)", 0.1, 5.0, 0.1)
-m2 = st.slider("m2 (kg)", 0.1, 5.0, 0.1)
-v1A = st.slider("v1 (m/s)", -10.0, 10.0, 0.0)
-v2A = st.slider("v2 (m/s)", -10.0, 10.0, 0.0)
+
+if "m1" not in st.session_state:
+    st.session_state["m1"] = 0.1
+if "m2" not in st.session_state:
+    st.session_state["m2"] = 0.1
+if "v1A" not in st.session_state:
+    st.session_state["v1A"] = 0.0
+if "v2A" not in st.session_state:
+    st.session_state["v2A"] = 0.0
 
 
-x_axis = []
+def on_slider_change():
+    st.session_state["m1"] = st.session_state["m1_slider"]
+    st.session_state["m2"] = st.session_state["m2_slider"]
+    st.session_state["v1A"] = st.session_state["v1A_slider"]
+    st.session_state["v2A"] = st.session_state["v2A_slider"]
+def on_input_change():
+    st.session_state["m1"] = st.session_state["m1_input"]
+    st.session_state["m2"] = st.session_state["m2_input"]
+    st.session_state["v1A"] = st.session_state["v1A_input"]
+    st.session_state["v2A"] = st.session_state["v2A_input"]
+
+
+col1, col2 = st.columns([3, 1])
+with col1:
+    st.slider("m1 (kg)", 0.1, 5.0, key="m1_slider", on_change=on_slider_change)
+    st.slider("m2 (kg)", 0.1, 5.0, key="m2_slider", on_change=on_slider_change)
+    st.slider("v1 (m/s)", -10.0, 10.0, key="v1A_slider", on_change=on_slider_change)
+    st.slider("v2 (m/s)", -10.0, 10.0, key="v2A_slider", on_change=on_slider_change)
+with col2:
+    st.number_input("m1 (kg)", 0.1, 5.0, key="m1_input", on_change=on_input_change)
+    st.number_input("m2 (kg)", 0.1, 5.0, key="m1_input", on_change=on_input_change)
+    st.number_input("v1 (m/s)", -10.0, 10.0, key="v1A_input", on_change=on_input_change)
+    st.number_input("v2 (m/s)", -10.0, 10.0, key="v2A_input", on_change=on_input_change)
+
+
+m1 = st.session_state["m1"] 
+m2 = st.session_state["m2"] 
+v1A = st.session_state["v1A"] 
+v2A = st.session_state["v2A"] 
+
 y_axis_v1B = []
 y_axis_v2B = []
 y_axis_deltaK = []
@@ -56,11 +86,8 @@ else:
     ax2.plot(x_axis, y_axis_deltaK, label="")
 
 ax2.set_xlabel("Hệ số phục hồi e.")       
-ax2.set_ylabel("% Động năng hao hụt (%).")
-if at_zero:
-    ax2.set_title(f"m1= {m1}(kg), m2= {m2}(kg), vận tốc tiến về 0.")
-else:
-    ax2.set_title(f"m1= {m1}(kg), m2= {m2}(kg), v'1= {v1A}(m/s), v'2= {v2A}(m/s)")       
+ax2.set_ylabel("% Động năng hao hụt (%).")       
+ax2.set_title(f"m1= {m1}(kg), m2= {m2}(kg), v'1= {v1A}(m/s), v'2= {v2A}(m/s)")       
 ax2.legend()            
 st.pyplot(fig2)
 
