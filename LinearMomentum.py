71ae84ff059d5bc65199a40e2d3a597c98b2e075
HuyLuong <binhluong1968@gmail.com>
2026-09-03 12:16:02 +0700

    Update LinearMomentum.py

--- LinearMomentum.py
+++ LinearMomentum.py
@@ -66,7 +66,7 @@ for index in range(0, 31):
         k1 = 0.5*(m1*pow(v1A, 2) + m2*pow(v2A, 2))
         k2 = 0.5*(m1*pow(v1B, 2) + m2*pow(v2B, 2))
         deltaKpercent = ((k1 - k2) / k1)*100
-    elif v1A == v1B:
+    if v1A == v1B:
         equals = True
         deltaKpercent = 0
 
