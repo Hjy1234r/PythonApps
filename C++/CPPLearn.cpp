205cfe399c3627bcbb51bdd1f2b6f66a097b2c32
Hjy1234r <binhluong1968@gmail.com>
2026-09-11 12:49:05 +0700

    added line

--- C++/CPPLearn.cpp
+++ C++/CPPLearn.cpp
@@ -31,12 +31,151 @@ int main() {
   cout << "The hypotenuse of the right triangle is: " << fixed
        << setprecision(2) << sqrt(pow(A, 2) + pow(B, 2)) << endl;
 #endif
-#if 1
+#if 0
   int radius;
   cout << "Enter the radius: ";
   cin >> radius;
+  cout << "The area of the circle is: "
+       << pow(radius, 2) * numbers::pi << endl;
   cout << "The circumference of the circle is: "
-       << pow(radius, 2) * std::numbers::pi << endl;
+       << 2*radius * numbers::pi << endl;
+#endif
+#if 0
+  int first, second, temp;
+  cout << "Input 1st number: ";
+  cin >> first;
+  cout << "Input 2nd number: ";
+  cin >> second;
+  temp = first;
+  first = second;
+  second = temp;
+  cout << "First number: " << first << endl
+       << "Second number: " << second << endl;
+#endif
+
+  // Chapter 3 Exercises:
+  // Check whether a number is positive or negative, even or odd
+#if 0
+  int input;
+  cout << "Enter a number: ";
+  cin >> input;
+  if (input > 0)
+    cout << "Number is positive\n";
+  else if (input < 0)
+    cout << "Number is negative\n";
+  else
+    cout << "Number is zero\n";
+
+  if (input % 2 == 0)
+    cout << "Number is even\n";
+  else
+    cout << "Number is odd\n";
+#endif
+  // Find the largest number among three
+#if 0
+  int n1, n2, n3, max = 1;
+  cout << "Enter 3 numbers: ";
+  cin >> n1 >> n2 >> n3;
+  if (n1 == n2 || n1 == n3 || n2 == n3)
+    cout << "Two or three numbers are equal.\n";
+  else if (n1 > n2) {
+    if (n1 > n3)
+      max = n1;
+    else
+      max = n3;
+  } else {
+    if (n2 > n3)
+      max = n2;
+    else
+      max = n3;
+  }
+  cout << "The largest number is " << max << endl;
+#endif
+  // Solve first degree equation
+#if 0
+  double a, b, result;
+  cout << "ax + b = 0\n" << "Enter the coefficients a and b: ";
+  cin >> a >> b;
+  if (a == 0 && b == 0)
+    cout << "Infinite Solutions.\n";
+  else if (a == 0 && b != 0)
+    cout << "No Solution.\n";
+  else {
+    result = -b / a;
+    cout << "X = " << result << endl;
+  }
+#endif
+  // Check for a right triangle
+#if 0
+  double a, b, c;
+  cout << "Enter three sides of a triangle: ";
+  cin >> a >> b >> c;
+  if (c == sqrt(a * a + b * b))
+    cout << "It is a right triangle!\n";
+  else if (a == sqrt(c * c + b * b))
+    cout << "It is a right triangle!\n";
+  else if (b == sqrt(a * a + c * c))
+    cout << "It is a right triangle!\n";
+  else
+    cout << "It is not a right triangle.\n";
+#endif
+
+  // Check for number of days in a month and year
+#if 0
+  int month, year, days;
+  bool is_leap;
+  cout << "Enter the month and year: ";
+  cin >> month >> year;
+  if (year % 4 == 0 && year % 100 != 0)
+    is_leap = true;
+  else if (year % 100 == 0 && year % 400 == 0)
+    is_leap = true;
+  else
+    is_leap = false;
+  switch (month) {
+  case 1:
+    days = 31;
+    break;
+  case 2:
+    if (is_leap)
+      days = 29;
+    else
+      days = 28;
+    break;
+  case 3:
+    days = 31;
+    break;
+  case 4:
+    days = 30;
+    break;
+  case 5:
+    days = 31;
+    break;
+  case 6:
+    days = 30;
+    break;
+  case 7:
+    days = 31;
+    break;
+  case 8:
+    days = 31;
+    break;
+  case 9:
+    days = 30;
+    break;
+  case 10:
+    days = 31;
+    break;
+  case 11:
+    days = 30;
+    break;
+  case 12:
+    days = 31;
+    break;
+  default:
+    cout << "Invalid month.\n";
+  }
+  cout << "Number of days: " << days << endl;
 #endif
   return 0;
 }
---
+++ C++/Testing.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <numbers>
+using namespace std;
+
+int main() {
+  double pi;
+  pi = numbers::pi;
+  cout << pi << endl;
+  return 0;
+}
